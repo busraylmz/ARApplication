cee4bf8d01808a3a22ed5aa160bb8d82b24f77af
Büşra Yılmaz <busra_ylmz1993@hotmail.com>
2021-11-12 04:33:56 +0300

    scene updated

--- Assets/Buttoncontrol.cs
+++ Assets/Buttoncontrol.cs
@@ -2,28 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class Buttoncontrol : MonoBehaviour
 {
-    public GameObject cube;
+	public Button Button_1;
+	public Button Button_2;
+	public Button Button_3;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	void Start()
+	{
+		
+		Button_1.GetComponent<Button>().onClick.AddListener(OnButton_1_Click);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		Button_2.GetComponent<Button>().onClick.AddListener(OnButton_2_Click);
 
-    public void OnButton_Pressed()
+		Button_3.GetComponent<Button>().onClick.AddListener(OnButton_3_Click);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("You have clicked the button!");
+	}
+
+	public void OnButton_1_Click()
     {
         Debug.Log("button_1");
-          SceneManager.LoadScene("MenDancersScene");
+        SceneManager.LoadScene("MenDancersScene");
 
-      //  cube.GetComponent<Material>().color = Color.blue;
     }
+
+	public void OnButton_2_Click()
+	{
+		Debug.Log("button_1");
+		SceneManager.LoadScene("WomenDancersScene");
+
+	}
+
+	public void OnButton_3_Click()
+	{
+		Debug.Log("button_1");
+		SceneManager.LoadScene("MenandWomenDancerScene");
+
+	}
 }
